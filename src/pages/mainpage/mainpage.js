5a627efc881d569a88b54b24287e6faf7d5c5e4c
Nikita-Schenikov <nikita-shenikov@mail.ru>
2024-04-14 01:28:00 +0300

    tune animation using react-animation-on-scroll

--- src/pages/mainpage/mainpage.js
+++ src/pages/mainpage/mainpage.js
@@ -4,6 +4,8 @@ import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import WorkIcon from '@mui/icons-material/Work';
 import pic1 from './../../img/shutterstock_1536927203.jpg'
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import "./mainpage.css"
 
 
@@ -15,36 +17,38 @@ export const Mainpage = () => {
                 <Slider/>
                 <div className="main-content">
                     <h1 className="head">ПРЕИМУЩЕСТВА</h1>
-                    <div className="items">
-                        <div className="item a">
-                            <AssignmentIcon style={{fontSize: "XXX-Large"}}/>
-                            <div className="item-description">Возможность покры-<br/>
-                            тий по техническому<br/>
-                            заданию заказчика<br/>
-                            сложных конструкций</div>
+                    <AnimationOnScroll animateIn="animate__bounceInRight" duration={2}>
+                        <div className="items">
+                            <div className="item a">
+                                <AssignmentIcon style={{fontSize: "XXX-Large"}}/>
+                                <div className="item-description">Возможность покры-<br/>
+                                тий по техническому<br/>
+                                заданию заказчика<br/>
+                                сложных конструкций</div>
+                            </div>
+                            <div className="item b">
+                                <LocalOfferIcon style={{fontSize: "XXX-Large"}}/>
+                                <div className="item-description">Гибкие цены<br/>
+                                на гальваническое<br/>
+                                покрытие</div>
+                            </div>
+                            <div className="item c">
+                                <AccessTimeIcon style={{fontSize: "XXX-Large"}}/>
+                                <div className="item-description">Исполнение заказа<br/>
+                                согласно срокам<br/>
+                                и договоренностью<br/>
+                                с клиентом</div>
+                            </div>
+                            <div className="item d">
+                                <WorkIcon style={{fontSize: "XXX-Large"}}/>
+                                <div className="item-description">Качественное<br/>
+                                гальваническое<br/>
+                                покрытие согласно<br/>
+                                ГОСТам</div>
+                            </div>
+                            <img className="e" src={pic1} style={{width: '220px', height: '300px'}} />
                         </div>
-                        <div className="item b">
-                            <LocalOfferIcon style={{fontSize: "XXX-Large"}}/>
-                            <div className="item-description">Гибкие цены<br/>
-                            на гальваническое<br/>
-                            покрытие</div>
-                        </div>
-                        <div className="item c">
-                            <AccessTimeIcon style={{fontSize: "XXX-Large"}}/>
-                            <div className="item-description">Исполнение заказа<br/>
-                            согласно срокам<br/>
-                            и договоренностью<br/>
-                            с клиентом</div>
-                        </div>
-                        <div className="item d">
-                            <WorkIcon style={{fontSize: "XXX-Large"}}/>
-                            <div className="item-description">Качественное<br/>
-                            гальваническое<br/>
-                            покрытие согласно<br/>
-                            ГОСТам</div>
-                        </div>
-                        <img className="e" src={pic1} style={{width: '220px', height: '300px'}} />
-                    </div>
+                    </AnimationOnScroll>
                 </div>
             </div>
         </div>
