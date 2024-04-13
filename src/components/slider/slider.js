df6248f4c35c84f063531435b28dda9652797b4b
Nikita-Schenikov <nikita-shenikov@mail.ru>
2024-04-13 19:51:36 +0300

    create slider

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,7 +1,22 @@
-
-import './slider'
+import sample from './../../videoplayback/IMG_1209.MP4';
+import './slider.css'
 
 
   export const Slider = () => {
-    <div></div>
+
+    let arrow = "------>"
+
+    return (
+          <div className='container'>
+              <video className='videoTag' autoPlay loop muted>
+                  <source src={sample} type='video/MP4' />
+              </video>
+              <div className='overlay-elements'>
+                <h1>ГАЛЬВАНИЧЕСКОЕ ПОКРЫТИЕ <br></br>
+                ОТ 49 РУБ ЗА ДМ2</h1>
+                <p>Нанесем покрытие. Проведем шлифовку, наплавку, полировку и гарцовку. Проконсультируем бесплатно.</p>
+                <a>Заказать консультацию {arrow}</a>
+              </div>
+          </div>
+    )
   }
