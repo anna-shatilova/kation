9d9c74ecdeb4f6d9ba38cba544e80d3628721f29
Anna Shatilova <anxet@mail.ru>
2024-04-19 16:53:16 +0300

    page gallery done

--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -1,6 +1,5 @@
 import {
   Breadcrumbs,
-  Button,
   Card,
   CardActionArea,
   CardActions,
@@ -11,6 +10,8 @@ import {
   Link,
   Typography,
 } from '@mui/material';
+import { Button } from '../../components/button/button';
+import './styles/gallery.css';
 
 const galleryFoto = [
   {
@@ -47,39 +48,49 @@ const galleryFoto = [
 
 export const Gallery = () => {
   return (
-    <Container sx={{ mt: 2 }}>
-      <Breadcrumbs separator="›" aria-label="breadcrumb" sx={{ mb: 2 }}>
+    <Container sx={{ mt: 2, bgcolor: 'background.wrap' }}>
+      <Breadcrumbs separator="›" aria-label="breadcrumb" sx={{ pt: 2 }}>
         <Link underline="hover" color="inherit" href="/">
           Главная
         </Link>
         <Typography color="inherit">Галерея</Typography>
       </Breadcrumbs>
-      <Typography variant="h2" color="inherit" sx={{ mb: 2 }}>
+      <Typography variant="h1" color="inherit" sx={{ mb: 2 }}>
         Галерея
       </Typography>
       <Grid container spacing={2}>
         {galleryFoto.map((elem) => (
           <Grid key={elem.id} item xs={12} md={3}>
-            <Card sx={{ height: '100%' }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={elem.url}
-                  alt="foto"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {elem.name}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" underline="hover" color="inherit" href="/">
-                  Подробнее
-                </Button>
-              </CardActions>
-            </Card>
+            <div className="card">
+              <Card
+                sx={{
+                  height: '100%',
+                  bgcolor: 'background.card',
+                }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={elem.url}
+                    alt="foto"
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      color="#FFFFFF"
+                      component="div"
+                    >
+                      {elem.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button />
+                </CardActions>
+              </Card>
+            </div>
           </Grid>
         ))}
       </Grid>
