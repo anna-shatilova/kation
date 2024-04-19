503caa4ebf082725527940ef85d0ac154e6d8a2c
Anna Shatilova <anxet@mail.ru>
2024-04-19 17:12:17 +0300

    create service Gallery

--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -12,39 +12,7 @@ import {
 } from '@mui/material';
 import { Button } from '../../components/button/button';
 import './styles/gallery.css';
-
-const galleryFoto = [
-  {
-    id: 1,
-    name: 'Фото работ по золочению',
-    url: '/img/gallery/slide6.jpg',
-  },
-  {
-    id: 2,
-    name: 'Фото работ по серебрению',
-    url: '/img/gallery/slide3.jpg',
-  },
-  {
-    id: 3,
-    name: 'Фото работ по меднению',
-    url: '/img/gallery/slide8.jpg',
-  },
-  {
-    id: 4,
-    name: 'Фото работ по хромированию',
-    url: '/img/gallery/slide11.jpg',
-  },
-  {
-    id: 5,
-    name: 'Фото работ по оловянированию',
-    url: '/img/gallery/slide12.jpg',
-  },
-  {
-    id: 6,
-    name: 'Фото работ по обработке пескоструйкой',
-    url: '/img/gallery/slide20.jpg',
-  },
-];
+import { serviceGallery } from '../../utilities/serviceGallery';
 
 export const Gallery = () => {
   return (
@@ -59,7 +27,7 @@ export const Gallery = () => {
         Галерея
       </Typography>
       <Grid container spacing={2}>
-        {galleryFoto.map((elem) => (
+        {serviceGallery.map((elem) => (
           <Grid key={elem.id} item xs={12} md={3}>
             <div className="card">
               <Card
---
+++ src/utilities/serviceGallery.js
@@ -0,0 +1,44 @@
+export const serviceGallery = [
+    {
+      id: 1,
+      name: 'Фото работ по золочению',
+      url: '/img/gallery/slide6.jpg',
+    },
+    {
+      id: 2,
+      name: 'Фото работ по серебрению',
+      url: '/img/gallery/slide3.jpg',
+    },
+    {
+      id: 3,
+      name: 'Фото работ по никелированию',
+      url: '/img/gallery/slide8.jpg',
+    },
+    {
+      id: 4,
+      name: 'Фото работ по черному никелированию',
+      url: '/img/gallery/slide11.jpg',
+    },
+    {
+      id: 5,
+      name: 'Фото работ по химникелированию',
+      url: '/img/gallery/slide12.jpg',
+    },
+    {
+      id: 6,
+      name: 'Фото работ по меднению',
+      url: '/img/gallery/slide20.jpg',
+    },
+    {
+      id: 7,
+      name: 'Фото работ по палладированию',
+      url: '/img/gallery/slide20.jpg',
+    },
+    {
+      id: 8,
+      name: 'Фото работ по оловянированию',
+      url: '/img/gallery/slide20.jpg',
+    },
+  
+  ];
+  
