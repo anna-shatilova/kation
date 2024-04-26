431551e4f5b924f13999dbe39bce57869bf90e68
Anna Shatilova <anxet@mail.ru>
2024-04-26 17:09:07 +0300

    Add Styled Components, deleted shared files CSS.

--- src/components/serviceCard/serviceCard.js
+++ src/components/serviceCard/serviceCard.js
@@ -1,16 +1,17 @@
-import { Button } from '../button/button'
-import './serviceCard.css'
+import { ButtonWithBorder } from '../../App.styles';
+// import { Button } from '../button/button'
+import './serviceCard.css';
 
-export const ServiceCard = ({background, name, descr}) => {
-    return (
-        <div className='cardBox'>
-            <div className='material' style={{background: background}}></div>
-            <div className='material-header'>
-                <h4>{name}</h4>
-            </div>
-            <div className='description'>{descr}</div>
-            {/* <button className='btn'>Подробнее</button> */}
-            <Button/>
-        </div>
-    )
-}
+export const ServiceCard = ({ background, name, descr }) => {
+  return (
+    <div className="cardBox">
+      <div className="material" style={{ background: background }}></div>
+      <div className="material-header">
+        <h4>{name}</h4>
+      </div>
+      <div className="description">{descr}</div>
+      {/* <button className='btn'>Подробнее</button> */}
+      <ButtonWithBorder>Подробнее</ButtonWithBorder>
+    </div>
+  );
+};
