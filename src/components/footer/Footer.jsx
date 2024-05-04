c872bfeb266a9c15a3bfd26db62b89cc5086d7fd
Anna Shatilova <anxet@mail.ru>
2024-05-04 23:21:28 +0300

    Add simple footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,96 +1,33 @@
-// import EmailIcon from '@mui/icons-material/Email';
-// import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
-// import { Box, CardMedia, Container, Link, Toolbar } from '@mui/material';
+import { Link } from 'react-router-dom';
+import * as S from './Footer.styles';
+import { ButtonBlue } from '../../App.styles';
 
 export const Footer = () => {
   return (
-    <></>
-    // <Box
-    //   component="footer"
-    //   sx={{
-    //     top: 'auto',
-    //     bottom: 0,
-    //     width: '100%',
-    //     height: 'auto',
-    //     bgcolor: 'background.card',
-    //     // flexGrow: 1,
-    //   }}
-    // >
-    //   <Container
-    //     maxWidth="xl"
-    //     // sx={{
-    //     //   display: 'flex',
-    //     //   flexDirection: 'row',
-    //     //   rowGap: 2,
-    //     // }}
-    //   >
-    //     <Toolbar disableGutters>
-    //       <CardMedia
-    //         component="img"
-    //         alt="logo"
-    //         image="/img/logo.svg"
-    //         sx={{ mt: 2, display: { xs: 'none', md: 'flex' }, width: 'auto' }}
-    //       />
-    //       <Toolbar sx={{ flexGrow: 1 }}>
-    //         <EmailIcon sx={{ color: 'text.svg' }} />
-    //         <Link
-    //           href="mailto:oookation@gmail.com"
-    //           color="inherit"
-    //           underline="none"
-    //           sx={{
-    //             '&:hover': {
-    //               color: 'action.hover',
-    //             },
-    //           }}
-    //         >
-    //           oookation@gmail.com
-    //         </Link>
-    //       </Toolbar>
-    //       <Toolbar sx={{ flexGrow: 1 }}>
-    //         <PhoneAndroidIcon sx={{ color: 'text.svg' }} />
-    //         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-    //           <Link
-    //             href="tel:+79032742878"
-    //             color="inherit"
-    //             underline="none"
-    //             sx={{
-    //               '&:hover': {
-    //                 color: 'action.hover',
-    //               },
-    //             }}
-    //           >
-    //             +7 903 274 2878
-    //           </Link>
-    //           <Link
-    //             href="tel:+79250750554"
-    //             color="inherit"
-    //             underline="none"
-    //             sx={{
-    //               '&:hover': {
-    //                 color: 'action.hover',
-    //               },
-    //             }}
-    //           >
-    //             +7 925 075 0554
-    //           </Link>
-    //         </Box>
-    //       </Toolbar>
-    //     </Toolbar>
-    //   </Container>
-    // </Box>
+    <S.Footer>
+      <S.FooterContent>
+        <Link to="/">
+          <S.FooterLogo src="/img//logo.svg" alt="logo" />
+        </Link>
+        <S.FooterMenu>
+          <S.FooterList>
+            <Link to="#">
+              <S.FooterListItem>Главная</S.FooterListItem>
+            </Link>
+            <Link to="#">
+              <S.FooterListItem>Услуги</S.FooterListItem>
+            </Link>
+            <Link to="#">
+              <S.FooterListItem>Галерея</S.FooterListItem>
+            </Link>
+            <Link to="#">
+              <S.FooterListItem>Контакты</S.FooterListItem>
+            </Link>
+          </S.FooterList>
+        </S.FooterMenu>
+        <ButtonBlue>Заказать консультацию</ButtonBlue>
+      </S.FooterContent>
+      <S.FooterText>© ООО Катион, 2024</S.FooterText>
+    </S.Footer>
   );
 };
-{
-  /* <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <PhoneAndroidIcon sx={{ color: 'text.svg' }} />
-        </Grid>
-        <Grid item xs={4}>
-          <br />
-        </Grid>
-      </Grid> */
-}
-{
-  /* </Container>
-      </Box> */
-}
