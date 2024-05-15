b26b4115dc905e8c86de0b5a5bd34ed4a7689029
Nikita-Schenikov <nikita-shenikov@mail.ru>
2024-05-15 19:56:20 +0300

    delete margin in slider

--- src/App.styles.js
+++ src/App.styles.js
@@ -25,8 +25,8 @@ body {
   -moz-osx-font-smoothing: grayscale;  
 //   width: 100%;
 //   height: 100%;
-  padding-right: 0;
-  overflow-x: hidden;
+  /* padding-right: 0;
+  overflow-x: hidden; */
 }
 
 input,
